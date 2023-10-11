98220fbc28e16e9b016f525119b9b6ee0531da94
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-11 04:54:01 -0700

    Hot fix

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,10 +7,15 @@
 
 int main(void)
 {
-printf("Size of a char: %lu byte(s)\n", sizeof(char));
-printf("Size of an int: %lu byte(s)\n", sizeof(int));
-printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-printf("Size of a flaot: %lu byte(s)\n", sizeof(float));
+int ca = sizeof(char);
+int in = sizeof(int);
+int loin = sizeof(long int);
+int loloin = sizeof(long long int);
+int flo = sizeof(float);
+printf("Size of a char: %dbyte(s)\n", ca);
+printf("Size of an int: %d byte(s)\n", in);
+printf("Size of a long int: %d byte(s)\n", loin);
+printf("Size of a long long int: %d byte(s)\n", loloin);
+printf("Size of a flaot: %d byte(s)\n", flo);
 return (0);
 }
