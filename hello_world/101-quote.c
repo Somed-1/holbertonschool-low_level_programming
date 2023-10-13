8be2072136441cc18663be66377f954debe9d836
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-12 21:50:02 -0700

    Hot

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, message, sizeof(message) - 1);
+char me[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, me, sizeof(me) - 1);
 return (1);
 }
