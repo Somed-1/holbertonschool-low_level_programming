39b34a70fdec9907a7e452334170c25ab2f70533
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-16 09:19:09 -0700

    New

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - returns 1 if char is lowercase
+ *
+ * @num: character to be checked if grater than zero etc, type int
+ *
+ * Return: 1 and prints + if > 0, 0 otherwise
+ **/
+int print_last_digit(int num);
+{
+int last;
+last = num % 10
+_putchar(48 + last);
+return (last);
+}
