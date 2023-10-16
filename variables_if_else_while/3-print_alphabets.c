4c816904d57f0a4927bf91d79bae830caa5221bb
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-16 05:02:36 -0700

    Hot

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -13,10 +13,9 @@ for (lower = 'a';lower<'z'+1;lower++)
 {
 	putchar(lower);
 }
-for (upper = 'A';upper<'Z'+1;upper++)
+for (upper = 'A';upper<'Z'+2;upper++)
 {
 	putchar(upper);
 }
-
 return (0);
 }
