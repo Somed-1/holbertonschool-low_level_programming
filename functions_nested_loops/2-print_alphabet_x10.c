99652a6f1d0b5f207f23da8c0d4a4926468f9f64
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-16 08:10:41 -0700

    Hot

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,20 @@
 #include "main.h"
 /**
+ * print_alphabet - Entry point
+ *
+ * Return: void (Success)
+ */
+void print_alphabet(void)
+{
+char new;
+for (new = 'a'; new <= 'z'; new++)
+{
+        _putchar(new);
+}
+_putchar('\n');
+}
+
+/**
  * print_alphabet_x10 - Entry point
  *
  * Return: void (Success)
