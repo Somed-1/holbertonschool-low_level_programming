bb925110f48ed6915f7f934d56e47735738284cb
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-16 09:38:53 -0700

    New

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * jack_bauer - returns 1 if char is lowercase
+ *
+ * Return: void
+ **/
+void jack_bauer(void)
+{
+	for (int hour = 0; hour < 24; hour++)
+	{
+		for (int minute = 0; minute < 60; minute++)
+		{
+			_putchar('0' + (hour / 10));
+			_putchar('0' + (hour % 10));
+			_putchar(':');
+			_putchar('0' + (minute / 10));
+			_putchar('0' + (minute % 10));
+			_putchar('\n');
+		}
+	}
+}
+
