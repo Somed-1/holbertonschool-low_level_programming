dd967e76b9c829d00dc418f0dd683750cca80796
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-16 08:54:07 -0700

    New

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - Entry point
+ *
+ * @n: int
+ *
+ * Return: 1 or 0 (Success)
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+	_putchar('+');
+	return (1);
+}
+else if (n == 0)
+{
+	_putchar(48);
+	return (0);
+}
+else if ()
+{
+	_putchar('-');
+	return (-1);
+}
+}
