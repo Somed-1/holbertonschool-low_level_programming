8a2a798f0a3be79b08da5e2e7a75f39d90b977f4
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-17 04:37:30 -0700

    Hot

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,29 +1,5 @@
+#include <stdio.h>
 #include "main.h"
-
-/**
- * print_number - returns 1 if char is lowercase
- *
- * @n: int
- *
- * Return: 1 if lower case, 0 otherwise
- **/
-
-void print_number(int n)
-{
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-	}
-
-	if (n / 10)
-	{
-		print(n / 10);
-	}
-
-	_putchar(n % 10 + '0')
-}
-
 /**
  * print_to_98 - returns 1 if char is lowercase
  *
@@ -31,16 +7,37 @@ void print_number(int n)
  *
  * Return: 1 if lower case, 0 otherwise
  **/
-
 void print_to_98(int n)
 {
-int last;
-int first;
-for (; n < 99; n++)
+if (n < 98)
 {
-	print(n);
-	_putchar(',');
-	_putchar(' ');
+while (n <= 98)
+{
+if (n == 98)
+{
+printf("%d", n);
 }
-_putchar('\n');
+else
+{
+printf("%d, ", n);
+}
+n++;
+}
+}
+else
+{
+while (n >= 98)
+{
+if (n == 98)
+{
+printf("%d", n);
+}
+else
+{
+printf("%d, ", n);
+}
+n--;
+}
+}
+printf("\n");
 }
