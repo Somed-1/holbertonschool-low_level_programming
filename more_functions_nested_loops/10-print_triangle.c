7a7457a16bf8f9aaf2d5997f72f25aeb535bef9f
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-17 09:03:13 -0700

    hot

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -11,6 +11,8 @@ void print_triangle(int size)
 int i;
 int j;
 int e;
+if (size > 0)
+{
 for (i = 1; i < size + 1; i++)
 {
 	for (j = 0; j < size - i; j++)
@@ -22,6 +24,11 @@ for (i = 1; i < size + 1; i++)
 	{
 		_putchar('#');
 	}
+	_putchar('\n');
+}
+}
+else
+{
 	_putchar('\n');
 }
 }
