2c962ed0405d0d4ddcc378994ae58dc3ab55ed37
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-17 03:07:23 -0700

    Hot

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -13,12 +13,13 @@ int j;
 int number;
 int last;
 
+
 for (i = 0; i < 10; i++)
 {
 	for (j = 0; j < 10; j++)
 	{
 		number = i * j;
-		if (col > 0)
+		if (j > 0)
 		{
 			_puthcar(',');
 			_putchar(' ');
