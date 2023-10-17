596b0ba031aad8161356b90391c1a0577b160612
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-17 06:58:16 -0700

    Hot

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isdigit - returns 1 if char is lowercase
+ *
+ * @c: character to be checked if upper case, type int
+ *
+ * Return: 1 if lower case, 0 otherwise
+ **/
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+{
+	return (1);
+}
+else
+{
+	return (0);
+}
+}
