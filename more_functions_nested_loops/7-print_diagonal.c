48de02a76c5de561b3eb64f2e9171c6794a75556
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-17 08:18:29 -0700

    Hot

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -2,24 +2,29 @@
 /**
  * print_diagonal - returns 1 if char is lowercase
  *
- *@n: int
+ * @n: character to be checked if grater than zero etc, type int
  *
- * Return: 1 if lower case, 0 otherwise
+ * Return: 1 and prints + if > 0, 0 otherwise
  **/
 void print_diagonal(int n)
 {
 int i;
-for (i = 0; i < n - 1; i++)
-{
-	if (n < 0)
-	{
-		continue;
-	}
-	_putchar(' ');
-}
+int j;
+
 if (n > 0)
 {
-	_putchar('\\');
+for (j = 0; j < n; j++)
+{
+for (i = 0; i < j; i++)
+{
+_putchar(' ');
 }
+_putchar('\\');
 _putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
 }
