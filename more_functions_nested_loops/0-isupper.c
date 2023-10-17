e6728b871b5db5c92377e75d3b10a2f6e0e64c2f
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-17 06:50:56 -0700

    New

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isuper - Entry point
+ *
+ *@c: int
+ *
+ * Return: Always 0 (Success)
+ */
+int _isuper(int c);
+{
+if (c >= 65 && c <= 90)
+{
+	return (1);
+}
+return (0);
+}
