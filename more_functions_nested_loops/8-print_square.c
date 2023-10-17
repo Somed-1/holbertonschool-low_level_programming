8708164ab94616104f3e9db80f258d3fa83302e2
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-17 08:28:39 -0700

    hot

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_square - returns 1 if char is lowercase
+ *
+ * @size: character to be checked if grater than zero etc, type int
+ *
+ * Return: 1 and prints + if > 0, 0 otherwise
+ **/
+void print_square(int size)
+{
+int i;
+int j;
+for (i = 0; i < size; i++)
+{
+	for (j = 0; j < size; j++)
+	{
+		_putchar('#');
+	}
+	_putchar('\n');
+}
+}
