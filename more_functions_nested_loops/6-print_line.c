578ae6bd7fbd966e8a4efd45cf77897041177637
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-17 07:43:17 -0700

    Hot

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_line - returns 1 if char is lowercase
+ * print_line i- returns 1 if char is lowercase
  *
  *@n: int
  *
@@ -11,6 +11,11 @@ void print_line(int n)
 int i;
 for (i = 0; i < n; i++)
 {
+	if (n < 0)
+	{
+		continue;
+	}
 	_putchar('_');
 }
+_putchar('\n');
 }
