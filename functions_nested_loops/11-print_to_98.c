c1632d945476a80a24651adea73abccd9ec62fa4
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-17 03:48:43 -0700

    Hot

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -11,14 +11,15 @@
 void print_to_98(int n)
 {
 int last;
+int first;
 for (; n < 99; n++)
 {
+	if (n < 0)
 	if (n >= 10)
 	{
 		last = n % 10;
-		n /= 10;
-		n %= 10;
-		_putchar('0' + n);
+		first = n / 10 % 10;
+		_putchar('0' + first);
 		_putchar('0' + last);
 	}
 	else
