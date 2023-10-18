ac6ee6fdb4b78fa17af7bfb07b93a1d170734332
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-18 04:14:56 -0700

    Hot

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -10,7 +10,7 @@ int number;
 int number_2;
 for (number = 0; number < 9; number++)
 {
-	for (number_2 = number + 1; number_2 < 10; number_2)
+	for (number_2 = number + 1; number_2 < 10; number_2++)
 	{
 		putchar('0' + number);
 		putchar('0' + number_2);
