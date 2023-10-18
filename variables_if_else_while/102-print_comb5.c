64b3687739fa4109337d31b262a45cbcc6b74317
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-18 04:46:40 -0700

    Hot

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -3,35 +3,31 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
- */
+*/
 int main(void)
 {
-int number;
-int number_2;
-int integer;
-int integer_2;
-for (number = 0; number < 10; number++)
+int i, j;
+
+for (i = 0; i <= 99; i++)
 {
-	for (number_2 = number; number_2 < 10; number_2++)
-	{
-		for (integer = number_2; integer < 10; integer++)
-		{
-			for (integer_2 = integer + 1; integer_2 < 10; integer_2++)
-			{
-				putchar('0' + number);
-				putchar('0' + number_2);
-				putchar(' ');
-				putchar('0' + integer);
-				putchar('0' + integer_2);
-				if (number != 8)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
-	}
+for (j = 0; j <= 99; j++)
+{
+if (i < j)
+{
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
+putchar(' ');
+putchar(j / 10 + '0');
+putchar(j % 10 + '0');
+if (i < 98)
+{
+putchar(',');
+putchar(' ');
 }
+}
+}
+}
+
 putchar('\n');
 return (0);
 }
