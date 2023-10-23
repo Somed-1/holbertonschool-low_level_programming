6e51ea120937190ed5827e3975c10cebc03d062b
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-23 04:51:26 -0700

    Hotfix

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strlen - Entry point
+ *
+ *@s: int
+ *
+ * Return: Always 0 (Success)
+ */
+int _strlen(char *s)
+{
+int i;
+i = 0;
+while (s[i] != '\0')
+{
+	i++;
+}
+return (i);
+}
+
+/**
+ * _print_rev - Entry point
+ *
+ *@s: int
+ *
+ * Return: Always 0 (Success)
+ */
+void _print_rev(char *s)
+{
+int i;
+i = _strlen(s) - 1;
+while (i > 0)
+{
+	_putchar(str[i]);
+	i--;
+}
+_putchar('\n');
+}
