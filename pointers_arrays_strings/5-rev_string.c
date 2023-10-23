5b1cbe655d12684aaee014a777b927733586cdfc
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-23 05:27:06 -0700

    Hot

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -9,20 +9,17 @@
  */
 void rev_string(char *s)
 {
-    int length = 0;
-    char temp;
-
-    if (s == NULL)
-        return;
-
-    while (s[length] != '\0')
-        length++;
-
-    for (int start = 0, end = length - 1; start < end; start++, end--)
-    {
-        temp = s[start];
-        s[start] = s[end];
-        s[end] = temp;
-    }
+int length = 0;
+char temp;
+while (s[length] != '\0')
+{
+	length++;
+	for (int start = 0, end = length - 1; start < end; start++, end--)
+	{
+		temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+	}
+}
 }
 
