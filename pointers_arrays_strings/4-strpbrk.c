934c4bdf803099d816cc382e7b1d298f8762eeaf
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-24 08:23:47 -0700

    New file 4-strpbrk.c

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * _strstr - Concatenate two strings, limiting the number of characters copied
+ *
+ * @haystack: Destination string
+ * @needle: Maximum number of characters to copy from src
+ *
+ * Return: Pointer to the concatenated string
+ */
