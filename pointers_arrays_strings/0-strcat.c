036d4feed2d33035238eff4f10f53956c3ecf5ad
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-24 04:18:53 -0700

    Hotfix

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strlen - Entry point
+ *
+ *@s: int
+ *
+ * Return: Always 0 (Success)
+ */
+int _strlen(char *s)
+{
+	int len;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * _strcat - Entry point
+ *
+ * @dest: char
+ * @src: char
+ *
+ * Return: Always 0 (Success)
+ */
+char *_strcat(char *dest, char *src)
+{
+	int dest_len = _strlen(dest);
+	int i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i+dest_len] = src[i];
+		i++;
+	}
+	return dest;
+}
