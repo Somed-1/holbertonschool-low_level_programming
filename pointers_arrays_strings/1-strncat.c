0f6017083fdd1c58f5c2306ee1f58ea9cb85b6bf
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-24 04:36:45 -0700

    Hotfat

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _strlen - Entry point
+ *
+ *@s: int
+ *
+ * Return: Always 0 (Success)
+ */
+int _strlen(char *s)
+{
+int len;
+len = 0;
+while (s[len] != '\0')
+{
+	len++;
+}
+return (len);
+}
+
+/**
+ * _strncat - Entry point
+ *
+ * @dest: char
+ * @src: char
+ * @n: int
+ *
+ * Return: Always 0 (Success)
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int dest_len = _strlen(dest);
+int i = 0;
+while (i < n)
+{
+	dest[i+dest_len] = src[i];
+	i++;
+}
+return dest;
+}
