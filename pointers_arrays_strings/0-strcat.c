05fca05aa81e8e330cbcf2d6105fb0637b7a48f3
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-24 04:23:44 -0700

    Lox

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -9,13 +9,13 @@
  */
 int _strlen(char *s)
 {
-	int len;
-	len = 0;
-	while (s[len] != '\0')
-	{
-		len++;
-	}
-	return (len);
+int len;
+len = 0;
+while (s[len] != '\0')
+{
+	len++;
+}
+return (len);
 }
 
 /**
@@ -28,12 +28,12 @@ int _strlen(char *s)
  */
 char *_strcat(char *dest, char *src)
 {
-	int dest_len = _strlen(dest);
-	int i = 0;
-	while (src[i] != '\0')
-	{
-		dest[i + dest_len] = src[i];
-		i++;
-	}
-	return dest;
+int dest_len = _strlen(dest);
+int i = 0;
+while (src[i] != '\0')
+{
+	dest[i + dest_len] = src[i];
+	i++;
+}
+return (dest);
 }
