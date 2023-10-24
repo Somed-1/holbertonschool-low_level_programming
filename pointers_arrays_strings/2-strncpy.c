6ccdf4bd3ac33bbd6b2e497cb5a4b2ee0bb8f5e9
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-24 04:54:28 -0700

    Hot fix

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncpy - Concatenate two strings, limiting the number of characters copied
+ *
+ * @dest: Destination string
+ * @src: Source string
+ * @n: Maximum number of characters to copy from src
+ *
+ * Return: Pointer to the concatenated string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i = 0;
+while (i < n && src[i] != '\0')
+{
+	dest[i] = src[i];
+	i++;
+}
+
+return (dest);
+}
+
