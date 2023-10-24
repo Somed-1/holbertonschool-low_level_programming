1559168e3bc1c05e253e3983a5ac44bbf595012a
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-24 04:44:58 -0700

    Hotfix

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,20 +1,15 @@
 #include "main.h"
 
 /**
- * _strlen - Entry point
+ * _strlen - Calculate the length of a string
  *
- *@s: int
+ * @s: Pointer to a string
  *
- * Return: Always 0 (Success)
+ * Return: The length of the string
  */
 int _strlen(char *s)
 {
-if (s[0] == '')
-{
-	return(0);
-}
-int len;
-len = 0;
+int len = 0;
 while (s[len] != '\0')
 {
 	len++;
@@ -23,22 +18,29 @@ return (len);
 }
 
 /**
- * _strncat - Entry point
+ * _strncat - Concatenate two strings, limiting the number of characters copied
  *
- * @dest: char
- * @src: char
- * @n: int
+ * @dest: Destination string
+ * @src: Source string
+ * @n: Maximum number of characters to copy from src
  *
- * Return: Always 0 (Success)
+ * Return: Pointer to the concatenated string
  */
 char *_strncat(char *dest, char *src, int n)
 {
 int dest_len = _strlen(dest);
 int i = 0;
-while (i < n)
+
+if (src != NULL && dest != NULL)
 {
-	dest[i + dest_len] = src[i];
-	i++;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i + dest_len] = src[i];
+		i++;
+	}
 }
+dest[i + dest_len] = '\0';
+
 return (dest);
 }
+
