0e792fd73c0f5816e546e45a7dfa387bba411b4c
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-24 08:55:38 -0700

    Hot fix

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_diagsums - Concatenate two strings, limiting
  *
@@ -9,12 +10,13 @@
  */
 void print_diagsums(int *a, int size)
 {
-int i, sum, sum2;
-sum = 0;
+int i, sum1, sum2;
+sum1 = 0;
 sum2 = 0;
 for (i = 0; i < size; i++)
 {
 	sum1 += a[i * size + i];
 	sum2 += a[i * size + (size - i - 1)];
 }
+printf("%d, %d\n", sum1, sum2);
 }
