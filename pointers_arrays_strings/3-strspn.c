0b31f54b770cf1c84a495b2c422a190242f1398d
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-24 08:18:36 -0700

    New file 3-strspn.c

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _strspn - Concatenate two strings, limiting the number of characters copied
+ *
+ * @s: Destination string
+ * @accept: Maximum number of characters to copy from src
+ *
+ * Return: Pointer to the concatenated string
+ */
