abf24666009dc259391dcfc0b5d49db961d32279
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-24 08:56:45 -0700

    New file

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -6,3 +6,17 @@
  *
  * Return: Pointer to the concatenated string
  */
+void print_chessboard(char (*a)[8])
+{
+int i, j;
+for (i = 0; i < 8; i++)
+{
+if (i != 0)
+_putchar('\n');
+for (j = 0; j < 8; j++)
+{
+_putchar(a[i][j]);
+}
+}
+_putchar('\n');
+}
