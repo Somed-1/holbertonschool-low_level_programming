b8430bb4bcabfebfed54227d2f896b18a4826971
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-24 08:36:32 -0700

    New file 5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -7,3 +7,23 @@
  *
  * Return: Pointer to the concatenated string
  */
+char *_strstr(char *haystack, char *needle)
+{
+int i, j, k;
+
+for (i = 0; haystack[i] != '\0'; i++)
+{
+for (k = i, j = 0; needle[j] != '\0'; j++, k++)
+{
+if (needle[j] != haystack[k] || haystack[k] == '\0')
+{
+break;
+}
+}
+if (needle[j] == '\0')
+{
+return (haystack + i);
+}
+}
+return (0);
+}
