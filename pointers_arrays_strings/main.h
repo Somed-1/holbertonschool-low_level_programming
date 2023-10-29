6aafbe09091e8409665262d973b9e13e50869ea6
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-28 23:39:28 -0700

    Hotfix

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -31,4 +31,5 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void set_string(char **s, char *to);
+void print_number(int n);
 #endif /* MAIN_H */
