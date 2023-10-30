729f270c8b61de985bc89b98aa947f02273ba973
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-30 04:51:13 -0700

    hello

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -10,13 +10,13 @@
  */
 void _print_rev_recursion(char *s)
 {
-if (*s != '\0')
+if (*s == '\0')
 {
-	_print_rev_recursion(s + 1);
-	_putchar(*s);
+	_putchar('\n');
 }
 else
 {
-	_putchar('\n');
+	 _print_rev_recursion(s + 1);
+	 _putchar(*s);
 }
 }
