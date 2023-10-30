fd937ab9a61e25c7819bd270afefcc545c901ea8
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-30 05:50:59 -0700

    hotfix

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ * _sqrt_recursion - Entry point
+ *
+ *@n: int
+ *
+ * Return: Always 0 (Success)
+ */
+int _sqrt_recursion(int n)
+{
+if (n * n == n)
+{
+	return (n);
+}
+_sqrt_recursion(n-1);
+}
