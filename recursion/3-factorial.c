35f30e67b5a44fcc7c493fa8c64860fa3571ec88
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-30 05:15:31 -0700

    hotfix

---
+++ recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+ * factorial - Entry point
+ *
+ *@s: int
+ *
+ * Return: Always 0 (Success)
+ */
+int factorial(int n)
+{
+if (*s == '\0')
+{
+	return (1);
+}
+else
+{
+	return (n * factorial(n + 1));
+}
+}
