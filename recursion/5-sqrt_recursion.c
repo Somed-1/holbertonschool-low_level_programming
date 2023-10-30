a3271d6651b9ac1a1f687921a11c909a8a6167bd
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-30 06:25:27 -0700

    hot

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,22 +1,33 @@
 #include "main.h"
-
-
 /**
- * _sqrt_recursion - Entry point
+ * result - Entry point
  *
- *@n: int
+ *@a: int
+ *@b: int
  *
  * Return: Always 0 (Success)
- */
+*/
+int result(int a, int b)
+{
+if (a * a == b)
+return (a);
+
+if (a * a > b)
+return (-1);
+
+return (result(a + 1, b));
+}
+/**
+* _sqrt_recursion - Entry point
+*
+*@n: int
+*
+* Return: Always 0 (Success)
+*/
 int _sqrt_recursion(int n)
 {
-if (n < 0)
-{
-	return (-1);
-}
-if (n * n == n)
-{
-	return (n);
-}
-_sqrt_recursion(n-1);
+if (n == 0)
+return (0);
+
+return (result(1, n));
 }
