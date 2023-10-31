552d858eeef09cf8e3b191a46645c021e9aed598
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-31 06:27:02 -0700

    hoftfixjldjal

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * This function prints characters from the first command-line argument.
+ *
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ *
+ * Return: 0 if successful
+ */
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%d\n", argc);
+return (0);
+}
