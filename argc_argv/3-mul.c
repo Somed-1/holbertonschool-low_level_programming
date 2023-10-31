2257c59f482d1a4597a6075455f5716412ead7c8
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-31 06:43:16 -0700

    hotfix

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -17,9 +17,10 @@ if (argc < 3)
 	printf("Error\n");
 	return (1);
 }
-
-int x = atoi(argv[1]);
-int y = atoi(argv[2]);
+int x;
+int y;
+x = atoi(argv[1]);
+y = atoi(argv[2]);
 printf("%d\n", x * y);
 
 return (0);
