333adc8068671f09b9781c984f605da319162a40
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-31 06:42:33 -0700

    hotfix

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * main - Entry point of the program
@@ -13,13 +12,15 @@
  */
 int main(int argc, char *argv[])
 {
-if (argc < 2)
+if (argc < 3)
 {
 	printf("Error\n");
 	return (1);
 }
+
 int x = atoi(argv[1]);
-int y =	atoi(argv[2]);
+int y = atoi(argv[2]);
 printf("%d\n", x * y);
+
 return (0);
 }
