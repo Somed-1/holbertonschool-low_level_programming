0e87cdc14ceffe79b63e997d434271b8355e5d1c
Somed-1 <abbasovmaqa159@gmail.com>
2023-10-31 06:44:00 -0700

    fu

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,13 +12,13 @@
  */
 int main(int argc, char *argv[])
 {
+int x;
+int y;
 if (argc < 3)
 {
 	printf("Error\n");
 	return (1);
 }
-int x;
-int y;
 x = atoi(argv[1]);
 y = atoi(argv[2]);
 printf("%d\n", x * y);
