7297577ad1404ff71ca8d91b8c022e0d95db152a
Somed-1 <abbasovmaqa159@gmail.com>
2023-11-01 10:18:36 -0700

    hello

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -1,50 +1,49 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <stdio.h>
 
-/**
- * main - main function
- *
- * @argc: idk
- * @argv: idk
- * Return: something bruh
- */
 int main(int argc, char *argv[])
 {
 int i;
 int cents[5] = {25, 10, 5, 2, 1};
 int count = 0;
 int number;
+
 if (argc <= 1)
 {
 printf("Error\n");
-return (1);
+return 1;
 }
-if (argv[1][0] == '-')
+
+if (isdigit(*argv[1]) || (argv[1][0] == '-' && isdigit(argv[1][1])))
 {
-	printf("0\n");
-	return (0);
+number = atoi(argv[1]);
 }
-if (isdigit(*argv[1]))
+else
 {
 printf("Error\n");
-return (1);
+return 1;
 }
-number = atoi(argv[1]);
+
+if (number < 0)
+{
+printf("0\n");
+return 0;
+}
 
 for (i = 0; i < 5; i++)
 {
 while (number - cents[i] >= 0)
 {
-		number -= cents[i];
-		count++;
+number -= cents[i];
+count++;
 }
-if (number == 0)
-{
-		break;
+if (number == 0) {
+break;
 }
 }
+
 printf("%d\n", count);
-return (count);
+return count;
 }
 
