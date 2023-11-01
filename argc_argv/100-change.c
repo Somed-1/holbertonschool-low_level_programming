0b136452d2a9be78f0e368d4602ea2240538d23a
Somed-1 <abbasovmaqa159@gmail.com>
2023-11-01 10:23:50 -0700

    hello

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -15,13 +15,11 @@ int i;
 int cents[5] = {25, 10, 5, 2, 1};
 int count = 0;
 int number;
-
 if (argc <= 1)
 {
 printf("Error\n");
 return (1);
 }
-
 if (isdigit(*argv[1]) || (argv[1][0] == '-' && isdigit(argv[1][1])))
 {
 number = atoi(argv[1]);
@@ -31,13 +29,11 @@ else
 printf("Error\n");
 return (1);
 }
-
 if (number < 0)
 {
 printf("0\n");
 return (0);
 }
-
 for (i = 0; i < 5; i++)
 {
 while (number - cents[i] >= 0)
@@ -45,11 +41,11 @@ while (number - cents[i] >= 0)
 number -= cents[i];
 count++;
 }
-if (number == 0) {
+if (number == 0)
+{
 break;
 }
 }
-
 printf("%d\n", count);
 return (count);
 }
