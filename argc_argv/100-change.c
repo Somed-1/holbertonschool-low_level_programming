4efd8900a191b6144c8d216088dd17ae9b8c7975
Somed-1 <abbasovmaqa159@gmail.com>
2023-11-01 09:40:17 -0700

    hello

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -12,6 +12,10 @@
  */
 int main(int argc, char *argv[])
 {
+int i;
+int cents[5] = {25, 10, 5, 2, 1};
+int count = 0;
+int number = atoi(argv[1]);
 if (argc <= 1)
 {
 	printf("Error\n");
@@ -21,15 +25,9 @@ if (!isdigit(*argv[1]))
 {
 	printf("Error\n");
 	return (1);
-}
-int cents[5] = {25, 10, 5, 2, 1};
-int count = 0;
-int number = atoi(argv[1]);
-if (number == 0)
 {
 	return (0);
 }
-int i;
 for (i = 0; i < 5; i++)
 {
 	while (number - cents[i] >= 0)
