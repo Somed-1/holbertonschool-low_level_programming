14f3f8c346308d98577b6170ce23e13c6de7c186
Somed-1 <abbasovmaqa159@gmail.com>
2023-11-06 04:10:15 -0800

    hello

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,10 +13,6 @@
 char *str_concat(char *s1, char *s2)
 {
 char *result;
-if (s1 == NULL && s2 == NULL)
-{
-	return (NULL);
-}
 result = (char *)malloc(strlen(s1) + strlen(s2) + 1);
 if (result == NULL)
 {
