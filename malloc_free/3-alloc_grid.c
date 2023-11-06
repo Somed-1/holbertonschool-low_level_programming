bd61ea50325b83249a31c950b4c40c7a1febb7bb
Somed-1 <abbasovmaqa159@gmail.com>
2023-11-06 04:38:54 -0800

    heloo

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -15,7 +15,11 @@ int **alloc_grid(int width, int height)
 int *result;
 int i;
 result = malloc(sizeof(int) * width * height);
-for (i = 0; i < width * height)
+if (result == NULL)
+{
+	return (NULL);
+}
+for (i = 0; i < width * height; i++)
 {
 	result[i] = 0;
 }
