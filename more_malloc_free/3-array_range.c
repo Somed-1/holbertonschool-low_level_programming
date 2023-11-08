5b10c22f3f9a7d31aadcd29eed385749ebb40087
Somed-1 <abbasovmaqa159@gmail.com>
2023-11-08 04:04:17 -0800

    hel

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/**
+ * array_range - a function that concatenates two strings.
+ *
+ * @min: first char
+ * @max: secound char
+ *
+ * Return: If the function fails, it should return NULL
+ */
+void *array_range(int min, int max)
+{
+int *result;
+int len = max - min;
+unsigned int i;
+
+result = malloc(max * sizeof(int));
+if (result == NULL)
+{
+	return (NULL);
+}
+for (i = 0; i <= len; i++)
+{
+result[i] = min;
+min++;
+}
+return (result);
+}
