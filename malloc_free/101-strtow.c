ac215c31705f7259d8ce67fa4fc774a3b93ae7c2
Somed-1 <abbasovmaqa159@gmail.com>
2023-11-12 01:22:45 -0800

    hellowrold

--- malloc_free/101-strtow.c
+++ malloc_free/101-strtow.c
@@ -1,93 +1,108 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
-#define MAX_WORD_LENGTH 100
-
-int is_space(char c) {
-	return c == ' ' || c == '\t' || c == '\n';
+/**
+ * words_count
+ * 
+ * @str: string
+ * 
+ * Return: count of words
+ */
+int words_count(char *str)
+{
+	int count;
+	count = 0;
+	while (*str)
+	{
+		if (*str != ' ')
+		{
+			count += 1;
+			while (*str != ' ' && *str != '\0')
+			{
+				str++;
+			}
+		}
+		else
+		{
+			str++;
+		}
+	}
+	return (count);
 }
 
-int count_words(char *str) {
-	int count = 0;
-	int in_word = 0;
-
-	while (*str) {
-		if (is_space(*str)) {
-			in_word = 0;
-		} else if (!in_word) {
-			in_word = 1;
-			count++;
-		}
+/**
+ * copy - copy the string
+ * 
+ * @str: string
+ * @size: len
+ * 
+ * Return: string
+ */
+char *copy(char *str, int len)
+{
+	char *new;
+	int i;
+	i = 0;
+	new = malloc(sizeof(char) * (len + 1));
+	while (i < len)
+	{
+		*(new + i) = *str;
+		i++;
 		str++;
 	}
-
-	return count;
+	*(new + i) = '\0';
+	return (new);
+}
+/**
+ * strtow - separates words from given string
+ * 
+ * @str: string
+ * 
+ * Return: new array with separated words
+ */
+char **strtow(char *str)
+{
+	char **result;
+	char *start;
+	int count;
+	int len, temp_len, i, j;
+	len = strlen(str);
+	/*Check if string is correct*/
+	if (str == NULL || len == 0)
+	{
+		return (NULL);
+	}
+	count = words_count(str);
+	result = malloc(sizeof(char *) * (count + 1));
+	/*Check if malloc worked correct*/
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+	/*Filling result with pointers*/
+	i = 0;
+	while (*(str + j))
+	{
+		if (*(str + j) != ' ')
+		{
+			temp_len = 0;
+			start = (str + j);
+			while (*(str + j) != ' ' && *(str + j) != '\0')
+			{
+				j++;
+				temp_len += 1;
+			}
+			result[i] = copy(start, temp_len);
+			i++;
+		}
+		else
+		{
+			j++;
+		}
+	}
+	result[i] = NULL;
+	return (result);
 }
 
-char **strtow(char *str) {
-	if (str == NULL || *str == '\0') {
-		return NULL;
-	}
 
-	int num_words = count_words(str);
-	if (num_words == 0) {
-		return NULL;
-	}
-
-	char **result = (char **)malloc((num_words + 1) * sizeof(char *));
-	if (result == NULL) {
-		return NULL;
-	}
-
-	int word_count = 0;
-	int in_word = 0;
-	char *start = str;
-
-	while (*str) {
-		if (is_space(*str)) {
-			if (in_word) {
-				int word_length = str - start;
-				result[word_count] = (char *)malloc((word_length + 1) * sizeof(char));
-				if (result[word_count] == NULL) {
-					// Allocation failed, free previously allocated memory
-					for (int i = 0; i < word_count; i++) {
-						free(result[i]);
-					}
-					free(result);
-					return NULL;
-				}
-
-				strncpy(result[word_count], start, word_length);
-				result[word_count][word_length] = '\0';
-				word_count++;
-				in_word = 0;
-			}
-		} else if (!in_word) {
-			start = str;
-			in_word = 1;
-		}
-		str++;
-	}
-
-	if (in_word) {
-		int word_length = str - start;
-		result[word_count] = (char *)malloc((word_length + 1) * sizeof(char));
-		if (result[word_count] == NULL) {
-			// Allocation failed, free previously allocated memory
-			for (int i = 0; i < word_count; i++) {
-				free(result[i]);
-			}
-			free(result);
-			return NULL;
-		}
-
-		strncpy(result[word_count], start, word_length);
-		result[word_count][word_length] = '\0';
-		word_count++;
-	}
-
-	result[word_count] = NULL; // Set the last element to NULL
-
-	return result;
-}
