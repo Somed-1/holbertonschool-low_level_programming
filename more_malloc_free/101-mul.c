b3ffd24897732e61ce6ebaa9b1a682376bb5cfa7
Somed-1 <abbasovmaqa159@gmail.com>
2023-11-12 08:04:47 -0800

    hello

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -14,7 +14,7 @@ int main(int argc, char **argv)
 {
 	char *a, *b;
 	int len_a, len_b, max_len;
-	char *result;
+	int *result;  // Use an array of integers to store the result
 	int i, j, carry, product;
 
 	if (argc != 3)
@@ -28,7 +28,7 @@ int main(int argc, char **argv)
 	len_a = strlen(a);
 	len_b = strlen(b);
 	max_len = len_a + len_b + 1;
-	result = malloc(sizeof(char) * max_len);
+	result = malloc(sizeof(int) * max_len);
 
 	if (result == NULL)
 	{
@@ -38,7 +38,7 @@ int main(int argc, char **argv)
 
 	for (i = 0; i < max_len; i++)
 	{
-		result[i] = '0';
+		result[i] = 0;
 	}
 
 	for (i = len_a - 1; i >= 0; i--)
@@ -46,22 +46,26 @@ int main(int argc, char **argv)
 		carry = 0;
 		for (j = len_b - 1; j >= 0; j--)
 		{
-			product = (a[i] - '0') * (b[j] - '0') + (result[i + j + 1] - '0') + carry;
+			product = (a[i] - '0') * (b[j] - '0') + result[i + j + 1] + carry;
 			carry = product / 10;
-			result[i + j + 1] = (product % 10) + '0';
+			result[i + j + 1] = product % 10;
 		}
 		result[i + j + 1] += carry;
 	}
 
-	result[max_len - 1] = '\0';
-
+	// Find the first non-zero digit
 	i = 0;
-	while (result[i] == '0')
+	while (i < max_len && result[i] == 0)
 	{
 		i++;
 	}
 
-	printf("%s\n", result + i);
+	// Print the result
+	for (; i < max_len; i++)
+	{
+		printf("%d", result[i]);
+	}
+	printf("\n");
 
 	free(result);
 	return 0;
