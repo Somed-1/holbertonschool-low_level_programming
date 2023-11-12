ae031f125b1ddfa012b1b3911c7092253d67bea2
Somed-1 <abbasovmaqa159@gmail.com>
2023-11-12 04:02:48 -0800

    hellowrold

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -31,7 +31,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
     new = malloc(new_size);
     if (new == NULL)
         return (NULL);
-    
+    p_new = new;
+    p_ptr = ptr;
     for (i = 0; i < old_size; i++)
     {
         p_new[i] = p_ptr[i];
