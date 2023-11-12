b12274abb61d7c4a51449c4a9e37f2d3d0b4168e
Somed-1 <abbasovmaqa159@gmail.com>
2023-11-12 01:34:54 -0800

    helloworld

--- malloc_free/101-strtow.c
+++ malloc_free/101-strtow.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 /**
- * words_count - counts words using spaces
+ * words_count
  * 
  * @str: string
  * 
@@ -11,24 +11,24 @@
  */
 int words_count(char *str)
 {
-	int count;
-	count = 0;
-	while (*str)
+int count;
+count = 0;
+while (*str)
+{
+	if (*str != ' ')
 	{
-		if (*str != ' ')
-		{
-			count += 1;
-			while (*str != ' ' && *str != '\0')
-			{
-				str++;
-			}
-		}
-		else
+		count += 1;
+		while (*str != ' ' && *str != '\0')
 		{
 			str++;
 		}
 	}
-	return (count);
+	else
+	{
+		str++;
+	}
+}
+return (count);
 }
 
 /**
@@ -41,18 +41,18 @@ int words_count(char *str)
  */
 char *copy(char *str, int len)
 {
-	char *new;
-	int i;
-	i = 0;
-	new = malloc(sizeof(char) * (len + 1));
-	while (i < len)
-	{
-		*(new + i) = *str;
-		i++;
-		str++;
-	}
-	*(new + i) = '\0';
-	return (new);
+char *new;
+int i;
+i = 0;
+new = malloc(sizeof(char) * (len + 1));
+while (i < len)
+{
+	*(new + i) = *str;
+	i++;
+	str++;
+}
+*(new + i) = '\0';
+return (new);
 }
 /**
  * strtow - separates words from given string
@@ -63,50 +63,45 @@ char *copy(char *str, int len)
  */
 char **strtow(char *str)
 {
-	char **result;
-	char *start;
-	int count;
-	int len, temp_len, i, j;
-	len = strlen(str);
-	/*Check if string is correct*/
-	if (str == NULL || len == 0)
+char **result;
+char *start;
+int count;
+int len, temp_len, i, j;
+len = strlen(str);
+/*Check if string is correct*/
+if (str == NULL || len == 0)
+{
+	return (NULL);
+}
+count = words_count(str);
+result = malloc(sizeof(char *) * (count + 1));
+/*Check if malloc worked correct*/
+if (result == NULL)
+{
+	return (NULL);
+}
+/*Filling result with pointers*/
+i = 0;
+while (*(str + j))
+{
+	if (*(str + j) != ' ')
 	{
-		return (NULL);
-	}
-	count = words_count(str);
-	if (count == 0)
-	{
-		return (NULL);
-	}
-	result = malloc(sizeof(char *) * (count + 1));
-	/*Check if malloc worked correct*/
-	if (result == NULL)
-	{
-		return (NULL);
-	}
-	/*Filling result with pointers*/
-	i = 0;
-	while (*(str + j))
-	{
-		if (*(str + j) != ' ')
-		{
-			temp_len = 0;
-			start = (str + j);
-			while (*(str + j) != ' ' && *(str + j) != '\0')
-			{
-				j++;
-				temp_len += 1;
-			}
-			result[i] = copy(start, temp_len);
-			i++;
-		}
-		else
+		temp_len = 0;
+		start = (str + j);
+		while (*(str + j) != ' ' && *(str + j) != '\0')
 		{
 			j++;
+			temp_len += 1;
 		}
+		result[i] = copy(start, temp_len);
+		i++;
 	}
-	result[i] = NULL;
-	return (result);
+	else
+	{
+		j++;
+	}
 }
-
+result[i] = NULL;
+return (result);
+}
 
