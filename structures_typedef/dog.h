8c6c80537953f92dae7477be4b78f7e14c52b322
Somed-1 <abbasovmaqa159@gmail.com>
2023-11-22 04:44:45 -0800

    hellow

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -7,12 +7,13 @@
  * @age: Age of the dog
  * @owner: The name of the owner
  */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
