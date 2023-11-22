7bafbd2defdb40e60e0adb6bbbd01a701ddc0656
Somed-1 <abbasovmaqa159@gmail.com>
2023-11-22 07:18:57 -0800

    hel

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - prints struct dog
+ * @d: struct inself
+ * Return: void
+ */
+void pritn_dog(struct dog *d)
+{
+	printf("Name: %s\nAge: %f\nOwner: %s", dog->name, dog->age, dog->owner);
+}
