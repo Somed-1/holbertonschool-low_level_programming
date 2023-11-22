f3d8987c0ea6e72183b767f33b284b715534b5e9
Somed-1 <abbasovmaqa159@gmail.com>
2023-11-22 07:34:55 -0800

    hel

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -13,6 +13,8 @@ void print_dog(struct dog *d)
 		char *name = d->name != NULL ? d->name : "(nil)";
 		double age = d->age;
 		char *owner = d->owner != NULL ? d->owner : "(nil)";
+
+
 		printf("Name: %s\nAge: %f\nOwner: %s\n", name, age, owner);
 	}
 }
