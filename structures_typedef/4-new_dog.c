70196fb60b133d6da5ad2b243286beed90b06f7d
Somed-1 <abbasovmaqa159@gmail.com>
2023-11-24 03:37:43 -0800

    hellowr

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -20,7 +20,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 
 	new_dog->name = malloc(sizeof(char) * (strlen(name) + 1));
-	new_dog->owner = malloc(sizeof(char) * strlen(owner) + 1));
+	new_dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
 	
 	if (new_dog->name == NULL)
 	{
