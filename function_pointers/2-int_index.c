60667165b1c9af0040dbf8fc9286e596cfb26791
Somed-1 <abbasovmaqa159@gmail.com>
2023-11-25 06:05:03 -0800

    HelloWorld

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,4 +1,6 @@
 #include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * int_index - function
@@ -10,11 +12,15 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
+	if (array == NULL)
+	{
+		return (-1);
+	}
 	if (size <= 0)
 	{
 		return (1);
 	}
-	i = 0;
+	i = -1;
 	while (i++ < size)
 	{
 		if (cmp(array[i]))
