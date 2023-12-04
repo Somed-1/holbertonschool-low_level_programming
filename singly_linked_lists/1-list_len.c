baf897e7093824ae6846bc1da0bec51ed3eb0537
Somed-1 <abbasovmaqa159@gmail.com>
2023-12-04 05:04:40 -0800

    hello

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -2,13 +2,13 @@
 
 /**
  * list_len - return the len of linked list
- * @first: the node of linked list
+ * @h: the first node of linked list
  * Return: the lenth of linked list;
  */
 size_t list_len(const list_t *h)
 {
 	size_t len = 0;
-	
+
 	while (h)
 	{
 		len++;
