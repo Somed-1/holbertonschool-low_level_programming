a95ce3c5f53426733be05f485e0551d1f52ecf32
Somed-1 <abbasovmaqa159@gmail.com>
2023-12-04 04:42:50 -0800

    hello

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include "linked_list.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_list - functoin prints linked list
+ * @h: first node of linked list
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t len = 0;
+	while (*(h->next))
+	{
+		(*(h->str)) ? printf("[%d] %s\n", strlen(h->str), h->str) : printf("[0] (nil)");
+		*h = h->next;
+		i++;
+	}
+	return (size_t);
+}
