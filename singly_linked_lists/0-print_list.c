7b2fac63258fe156f09808a238110aea0efe3dda
Somed-1 <abbasovmaqa159@gmail.com>
2023-12-04 04:51:30 -0800

    hello

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -8,7 +8,7 @@
 size_t print_list(const list_t *h)
 {
 	size_t len = 0;
-	while (*(h->next))
+	while (*(h->next) != NULL)
 	{
 		(*(h->str)) ? printf("[%d] %s\n", strlen(h->str), h->str) : printf("[0] (nil)");
 		*h = h->next;
