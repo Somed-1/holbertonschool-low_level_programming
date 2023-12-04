4365adbb7dd2863d2914c522de41ee3176b4715b
Somed-1 <abbasovmaqa159@gmail.com>
2023-12-04 04:50:06 -0800

    hello

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,6 +1,10 @@
 #ifndef LINKED_LIST
 #define LINKED_LIST
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
