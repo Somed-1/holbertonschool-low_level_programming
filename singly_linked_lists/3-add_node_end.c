decc31f702e431364066e3156b2751cd16bde186
Somed-1 <abbasovmaqa159@gmail.com>
2023-12-05 04:54:47 -0800

    hello

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -15,11 +15,17 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 
 	new->str = strdup(str);
+
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
 	new->len = strlen(str);
-	
+	new-next = NULL;
+
 	if (*head == NULL)
 	{
-		new->next = NULL;
 		*head = new;
 		return (new);
 	}
