73626a4337845534bf39eb7d739b46c82056fb0d
Somed-1 <abbasovmaqa159@gmail.com>
2023-12-08 07:16:09 -0800

    hel

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -9,6 +9,10 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int shift = 1;
+	unsigned long int max = sizeof(unsigned long int) * 8 - 1;
+
+	if (max < index)
+		return (-1);
 	shift <<= index;
 	*n |= shift;
 	return (1);
