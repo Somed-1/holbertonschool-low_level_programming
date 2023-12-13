4f2fe36423d13488cdfc3106749e5e1dfe476fd6
Somed-1 <abbasovmaqa159@gmail.com>
2023-12-13 04:53:47 -0800

    Hot fix

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -15,16 +15,21 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (!filename)
 		return (0);
+
 	fd = open(filename, O_RDONLY);
+
 	if (fd < 0)
 		return (0);
+
 	buf = malloc(sizeof(char) * letters);
 	if (!buf)
 		return (0);
+
 	r_len = read(fd, buf, letters);
 	if (r_len < 0)
 		return (0);
-	w_len = write(1, buf, r_len);
+
+	w_len = write(STDOUT_FILENO, buf, r_len);
 	if (w_len < 0)
 		return (0);
 
