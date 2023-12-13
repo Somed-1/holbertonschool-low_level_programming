fde7de16e4521c57d064e3eb38ef4acf53b69aff
Somed-1 <abbasovmaqa159@gmail.com>
2023-12-13 05:04:39 -0800

    Hot fix

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -9,18 +9,21 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t w_len;
+	ssize_t w_len, text_len;
 
 	if (!filename)
 		return (-1);
 	if (!text_content)
+	{
 		text_content = " ";
+		text_len = 1;
+	}
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
 
-	w_len = write(fd, text_content, 1);
+	w_len = write(fd, text_content, text_len);
 	if (w_len < 0)
 		return (-1);
 
