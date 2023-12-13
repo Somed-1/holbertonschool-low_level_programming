1c1d0c4795d74800bfeb53bda3b456e9fc0fb9f6
Somed-1 <abbasovmaqa159@gmail.com>
2023-12-13 09:20:04 -0800

    New file

---
+++ file_io/3-cp.c
@@ -0,0 +1,84 @@
+#include "main.h"
+
+#define BUF_SIZE 1024
+
+#define CLOSE_ERR "Error: Can't close fd %d\n"
+#define OPEN_ERR "Error: Can't read from file %s\n"
+#define WRITE_ERR "Error: Can't write to %s\n"
+#define USAGE_ERR "Usage: cp file_from file_to\n"
+/**
+ * try_close - treis to close file
+ * @fd: fd of file
+ * Return: void
+ */
+void try_close(int fd)
+{
+	if (close(fd) < 0)
+	{
+		dprintf(STDERR_FILENO, CLOSE_ERR, fd);
+		exit(100);
+	}
+}
+
+/**
+ * check_fd - checks fd of file
+ * @fd_from: fd from
+ * @fd_to: fd to
+ * @file_from: file from
+ * @file_to: file to
+ * Return: void
+ */
+void check_fd(int fd_from, int fd_to, char *file_from, char *file_to)
+{
+	char *wrong;
+
+	if (fd_from < 0 || fd_to << 0)
+	{
+		wrong = (fd_from < 0) ? file_from : file_to;
+		dprintf(STDERR_FILENO, OPEN_ERR, wrong);
+		if (fd_from > 0)
+			close(fd_from);
+		else
+			close(fd_to);
+		exit(98);
+	}
+}
+
+/**
+ * main - main function
+ * @argc: num of args
+ * @argv: list of args
+ * Return: 0 on success
+ */
+int main(int argc, char **argv)
+{
+	int fd_from, fd_to;
+	char *file_from, *file_to;
+	char buf[BUF_SIZE] = {0};
+	int r_len, w_len;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, USAGE_ERR);
+		exit(98);
+	}
+	file_from = argv[1];
+	file_to = argv[2];
+
+	fd_from = open(file_from, O_RDONLY);
+	fd_to = open(file_to, 0664);
+	check_fd(fd_from, fd_to, file_from, file_to);
+
+	while ((r_len = read(fd_from, buf, BUF_SIZE)) == BUF_SIZE)
+	{
+		w_len = wirte(fd_to, buf, BUF_SIZE);
+		if (w_len < 0)
+		{
+			dprintf(STDERR_FILEENO, WRITE_ERR, file_to);
+			exit(99);
+		}
+	}
+	try_close(fd_from);
+	try_close(fd_to);
+	return (0);
+}
