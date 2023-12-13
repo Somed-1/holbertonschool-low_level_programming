fdcf7aed07c790c0084afe4905b0b7d7bc4f8591
Somed-1 <abbasovmaqa159@gmail.com>
2023-12-13 09:45:34 -0800

    Hot fix

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -20,26 +20,33 @@ void try_close(int fd)
 	}
 }
 
+
 /**
- * check_fd - checks fd of file
- * @fd_from: fd from
+ * check_to - checks to
  * @fd_to: fd to
- * @file_from: file from
  * @file_to: file to
  * Return: void
  */
-void check_fd(int fd_from, int fd_to, char *file_from, char *file_to)
+void check_to(int fd_to, char *file_to)
 {
-	char *wrong;
-
-	if (fd_from < 0 || fd_to < 0)
+	if (fd_to < 0)
 	{
-		wrong = (fd_from < 0) ? file_from : file_to;
-		dprintf(STDERR_FILENO, OPEN_ERR, wrong);
-		if (fd_from > 0)
-			close(fd_from);
-		else
-			close(fd_to);
+		dprintf(STDERR_FILENO, WRITE_ERR, file_to);
+		exit(99);
+	}
+}
+
+/**
+ * check_from - checks from
+ * @fd_from: fd from
+ * @file_from: file from
+ * Return: void
+ */
+void check_from(int fd_from, char *file_from)
+{
+	if (fd_from < 0)
+	{
+		dprintf(STDERR_FILENO, READ_ERR, file_from);
 		exit(98);
 	}
 }
@@ -66,8 +73,9 @@ int main(int argc, char **argv)
 	file_to = argv[2];
 
 	fd_from = open(file_from, O_RDONLY);
+	check_from(fd_from, file_from);
 	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	check_fd(fd_from, fd_to, file_from, file_to);
+	check_to(fd_to, file_to);
 
 	while ((r_len = read(fd_from, buf, BUF_SIZE)) > 0)
 	{
