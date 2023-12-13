a669b5635ce1a3baf152745d6006079100f830b6
Somed-1 <abbasovmaqa159@gmail.com>
2023-12-13 05:02:51 -0800

    Hot fix

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,12 +13,14 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
+	if (!text_content)
+		text_content = " ";
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
 
-	w_len = write(fd, text_content, strlen(text_content));
+	w_len = write(fd, text_content, 1);
 	if (w_len < 0)
 		return (-1);
 
