d30b18362ebe8e0809756d5df3b0dde1695d43cc
Somed-1 <abbasovmaqa159@gmail.com>
2023-12-13 09:37:33 -0800

    New file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -32,7 +32,7 @@ void check_fd(int fd_from, int fd_to, char *file_from, char *file_to)
 {
 	char *wrong;
 
-	if (fd_from < 0 || fd_to << 0)
+	if (fd_from < 0 || fd_to < 0)
 	{
 		wrong = (fd_from < 0) ? file_from : file_to;
 		dprintf(STDERR_FILENO, OPEN_ERR, wrong);
@@ -66,15 +66,15 @@ int main(int argc, char **argv)
 	file_to = argv[2];
 
 	fd_from = open(file_from, O_RDONLY);
-	fd_to = open(file_to, 0664);
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	check_fd(fd_from, fd_to, file_from, file_to);
 
-	while ((r_len = read(fd_from, buf, BUF_SIZE)) == BUF_SIZE)
+	while ((r_len = read(fd_from, buf, BUF_SIZE)) > 0)
 	{
-		w_len = wirte(fd_to, buf, BUF_SIZE);
+		w_len = write(fd_to, buf, r_len);
 		if (w_len < 0)
 		{
-			dprintf(STDERR_FILEENO, WRITE_ERR, file_to);
+			dprintf(STDERR_FILENO, WRITE_ERR, file_to);
 			exit(99);
 		}
 	}
