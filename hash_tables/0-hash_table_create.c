3305127ce80dde30bbcd93f4c0736bdf58b1f9a1
Somed-1 <abbasovmaqa159@gmail.com>
2023-12-14 01:19:26 -0800

    New file

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -13,8 +13,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (!new)
 		return (NULL);
 	new->size = size;
-	new->arr = malloc(sizeof(hash_node_t *) * size);
-	if (!(new->arr))
+	new->array = malloc(sizeof(hash_node_t *) * size);
+	if (!(new->array))
 		return (NULL);
 
 	return (new);
