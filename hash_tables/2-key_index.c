67615c206674702eaa79b6efe0d3ff65e3af3830
Somed-1 <abbasovmaqa159@gmail.com>
2023-12-14 07:11:36 -0800

    New file

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -9,5 +9,5 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	return (unsigned long int)(hash_djb2(key) % size);
+	return ((unsigned long int)(hash_djb2(key) % size));
 }
