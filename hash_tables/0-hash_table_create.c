8e7b3c4e293287f6b991a9dbaff6baca802fcf80
Somed-1 <abbasovmaqa159@gmail.com>
2023-12-14 01:11:51 -0800

    New file

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of array
+ * Return: pointers to new hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new;
+
+	new = malloc(sizeof(hash_table_t));
+	if (!new)
+		return (NULL);
+	new->size = size;
+	return (new);
+}
