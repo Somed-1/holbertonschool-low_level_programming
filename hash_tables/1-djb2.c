4e135177c2d8d3571c4aba4824362980f2047061
Somed-1 <abbasovmaqa159@gmail.com>
2023-12-14 05:36:48 -0800

    New file

---
+++ hash_tables/1-djb2.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+/**
+ * djb2 - hash function
+ * @str: string to hash
+ * Return: result of hash funciton
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	hash_value = 5381;
+
+	while (*str++)
+	{
+		hash_value = ((hash << 5) + hash + *str);
+	}
+	return hash;
+}
