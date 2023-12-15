19bd3ba1895f802d61c4310e780dd4a6dcdc71d2
Somed-1 <abbasovmaqa159@gmail.com>
2023-12-15 03:50:30 -0800

    hei

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -1,42 +1,28 @@
 #include "hash_tables.h"
 
 /**
- * linked_list_print - prints linked list
- * @head: first node
- * @comma: flag for commas
- * Return: void
- */
-void linked_list_print(hash_node_t *head, int comma)
-{
-	while (head)
-	{
-		printf("\'%s\':%d \'%s\'", head->key, comma,  head->value);
-		if (comma != 0)
-			printf(", ");
-		head = head->next;
-	}	
-}
-
-/**
  * hash_table_print - prints hash table
  * @ht: hash table
  * Return: void
  */
 void hash_table_print(const hash_table_t *ht)
 {
+	long unsigned int i = 0;
 	hash_node_t *cur;
-	long unsigned int i;
-	int comma = 1;
+	char *comma = "";
 
-	i = 0;
 	printf("{");
-	while (i < ht->size)
+	cur = ht->array[i];
+	printf("%s'%s': '%s'", comma, head->key, head->value);
+	comma = ", ";
+	while (i++ < ht->size)
 	{
+		while (cur)
+		{
+			printf("%s'%s': '%s'", comma, head->key, head->value);
+			cur = cur->next;
+		}
 		cur = ht->array[i];
-		if ((i + 1) == ht->size)
-			comma = 0;
-		linked_list_print(cur, comma);
-		i++;
 	}
 	printf("}\n");
 }
