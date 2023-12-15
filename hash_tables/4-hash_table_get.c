965bac9e4aeb21ee6c657fc8d120bc154e151594
Somed-1 <abbasovmaqa159@gmail.com>
2023-12-15 00:46:01 -0800

    hotfix

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -11,6 +11,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int idx;
 	hash_node_t *cur;
 
+	if (!ht)
+		return (NULL);
+
 	idx = key_index((const unsigned char *)key, ht->size);
 	cur = ht->array[idx];
 	while (cur)
