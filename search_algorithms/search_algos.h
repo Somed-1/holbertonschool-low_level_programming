cc71c73f68fc26b738b9dc95b545aadd6691d40c
Somed-1 <abbasovmaqa159@gmail.com>
2024-01-19 06:59:27 -0800

    Fix the header file

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -6,5 +6,7 @@
 
 /* tasks */
 int linear_search(int *array, size_t size, int value);
+void print_array(int *array, int left, int right);
+int binary_search(int *array, size_t size, int value);
 
 #endif
