efd64ee34128177dec2bf5db181998d83b46b9bd
Somed-1 <abbasovmaqa159@gmail.com>
2024-01-19 07:00:45 -0800

    Fix NULL

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -14,6 +14,11 @@ int binary_search(int *array, size_t size, int value)
 	int middle;
 	size_t k;
 
+	if (!array)
+	{
+		return (-1);
+	}
+
 	while (right >= left)
 	{
 		middle = left + (right - left) / 2;
