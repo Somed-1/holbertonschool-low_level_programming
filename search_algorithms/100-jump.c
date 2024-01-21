f76de7f68d3ee2e546129ba43757430c2c108e60
Somed-1 <abbasovamaqa159@gmail.com>
2024-01-22 01:18:06 +0400

    Fix algorithm

--- search_algorithms/100-jump.c
+++ search_algorithms/100-jump.c
@@ -23,19 +23,19 @@ int jump_search(int *array, size_t size, int value)
 		i += step;
 	}
 	
-	if (i > size)
-		return (-1);
-
 	printf("Value found between indexes [%d] and [%d]\n", i - step, i);
 
-	j = i;
-	while (j > 0 && value < array[j])
+	j = i - step;
+	while (j < i && value > array[j] && j < size)
 	{
 		printf("Value checked array[%d] = [%d]\n", j, array[j]);
-		j--;
+		j++;
 	}
 	
 	if (array[j] == value)
+	{
+		printf("Value checked array[%d] = [%d]\n", j, array[j]);
 		return (j);
+	}
 	return (-1);
 }
